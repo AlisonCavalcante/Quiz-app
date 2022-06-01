9ea434e8f95eeeed553b221f1a1aec2529a6dc58
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-06-01 17:00:07 -0300

    mudança no cronometro para interval

--- quiz/src/app/pages/home/home.component.ts
+++ quiz/src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { interval } from 'rxjs';
 import { PerguntasService } from 'src/app/services/perguntas.service';
 
 @Component({
@@ -10,37 +10,37 @@ import { PerguntasService } from 'src/app/services/perguntas.service';
 export class HomeComponent implements OnInit {
   perguntas!: any[];
   pergunta!: any;
-  index!: number;
   pontos: number = 0;
   time: number = 10;
   userName: string = '';
   finish: boolean = false;
-  constructor(
-    private perguntasService: PerguntasService,
-    private router: Router
-  ) {}
+  perguntaCorrente: number = 0;
+  totalPerguntas!: number;
+  interval$!: any;
+  constructor(private perguntasService: PerguntasService) {}
 
   ngOnInit(): void {
     this.perguntasService.get().subscribe((res) => {
       this.perguntas = res;
+      this.totalPerguntas = this.perguntas.length;
       this.getPerguntaAleatoria();
     });
-    this.initTime();
+    this.initTimer();
     this.getUser();
   }
 
   getPerguntaAleatoria() {
-    this.index = this.perguntas.length;
-    let random = Math.floor(Math.random() * (this.index - 0)) + 0;
+    let random = Math.floor(Math.random() * (this.perguntas.length - 0)) + 0;
     this.pergunta = this.perguntas[random];
     this.perguntas.splice(random, 1);
+    this.perguntaCorrente++;
   }
 
   validarResposta(opcaoCorreta: string, resposta: string) {
     if (resposta == opcaoCorreta) {
       this.pontos += 10;
       this.proximaPergunta();
-    } else if (resposta != opcaoCorreta) {
+    } else {
       this.proximaPergunta();
       if (this.pontos > 0) {
         this.pontos -= 10;
@@ -50,23 +50,40 @@ export class HomeComponent implements OnInit {
 
   proximaPergunta() {
     if (this.perguntas.length != 0) {
+      this.resetTimer();
       this.getPerguntaAleatoria();
-      this.initTime();
     } else {
       this.finish = true;
+      this.stopTimer();
     }
   }
 
-  initTime() {
-    if (this.time > 0) {
-      setTimeout(() => {
-        this.time -= 1;
-        this.initTime();
-      }, 1000);
-    } else {
-      this.time = 10;
-      this.proximaPergunta();
-    }
+  initTimer() {
+    this.interval$ = interval(1000).subscribe((val) => {
+      this.time -= 1;
+      if (this.time === 0) {
+        this.time = 10;
+        this.proximaPergunta();
+        if (this.pontos > 0) {
+          this.pontos -= 10;
+        }
+      }
+    });
+    setTimeout(() => {
+      this.interval$.unsubscribe();
+      this.finish = true;
+    }, 6000000);
+  }
+
+  stopTimer() {
+    this.interval$.unsubscribe();
+    this.time = 0;
+  }
+
+  resetTimer() {
+    this.stopTimer();
+    this.time = 10;
+    this.initTimer();
   }
 
   getUser() {
